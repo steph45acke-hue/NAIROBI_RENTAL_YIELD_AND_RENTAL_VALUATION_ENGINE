769358e250560b3a6babd5a8ccef6f5559190045
steph45acke-hue <steph45.ac.ke@gmail.com>
2026-09-10 21:54:42 +0300

    Add project screenshots and machine learning model script

--- diagnose_regression.py
+++ diagnose_regression.py
@@ -6,6 +6,9 @@ from sqlalchemy import create_engine
 def run_statistical_diagnostics():
     print("--- Connecting to 3NF Database for statistical Diagnostics ---")
 
+
+    #connecting to my sql database
+
     engine = create_engine("mysql+mysqlconnector://root:stephen0111301468@localhost/nairobi_real_estate")
 
     query = """
@@ -23,4 +26,23 @@ def run_statistical_diagnostics():
                 """
     df = pd.read_sql(query,con = engine)
     print(f"Loaded {len(df)} records for regression analysis.")
+ 
+    features_df = df[['bedrooms', 'size_sqm', 'monthly_rent_kes', 'property_type', 'location']]
+
+    x = pd.get_dummies(features_df,drop_first = True)
+    x= x.astype(float)
+
+    y = df['purchase_price_kes']
+
+    x_with_const = sm.add_constant(x)
+
+    ols_model = sm.OLS(y, x_with_const).fit()
+
+    print("\n" + "="*80)
+    print("OLS REGRESSION & HYPOTHESIS TESTING DIAGNOSTIC REPORT")
+    print("="*80)
+    print(ols_model.summary())
+    print("="*80)
     
+if __name__ == "__main__":
+    run_statistical_diagnostics()
